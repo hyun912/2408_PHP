27cfe42208e7458122af4a7f08452157a3e728c0
hyun912 <kim374523@naver.com>
2024-10-04 20:12:37 +0900

    241004 저장분

--- side_project/pepe_in_the_well/doc/design/js/common.js
+++ side_project/pepe_in_the_well/doc/design/js/common.js
@@ -13,6 +13,10 @@ const sync = () => {
   document.documentElement.dataset.theme = darkNow ? "light" : "dark";
   //   heading.innerText = `Now with ${darkNow ? "Dark" : "Light"} Mode.`;
   button.setAttribute("aria-pressed", darkNow ? true : false);
+
+  // 딱보니 light: true, dark: false 임
+  // 페이지 이동&새로고침시 초기화를 막기위해 저장
+  localStorage.setItem("theme", darkNow ? "light" : "dark");
 };
 
 const handleSync = () => {
@@ -21,3 +25,10 @@ const handleSync = () => {
 };
 
 button.addEventListener("click", handleSync);
+
+// 다크모드 상태에서 페이지 이동&새로고침시 저장된걸 불러옴
+if (localStorage.getItem("theme") === "dark") {
+  document.documentElement.dataset.theme = "dark";
+  button.setAttribute("aria-pressed", false);
+}
+// else{} // light값은 처리안해도 되냐고? 차피 냅둬도 초기값으로 세팅되는데 굳이??
--- side_project/pepe_in_the_well/doc/design/js/index.js
+++ side_project/pepe_in_the_well/doc/design/js/index.js
@@ -1,5 +1,5 @@
 // 게시글 썸네일 불러오기용 함수
 function loadThumb(id, name) {
-  // 해당 번호의 css 배경을 이름에 맞게 경로맞춰 넣음
+  // 해당 번호의 css 배경을 이름에 맞게 경로에 맞춰서 넣음
   $("#thumb-" + id).css("background-image", "url('./img/pcons/" + name + "')");
 }
