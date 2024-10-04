f8d3acacc57854271c39bc7ac0f296b602c16e90
hyun912 <kim374523@naver.com>
2024-10-05 01:29:30 +0900

    241005 저장분

---
+++ side_project/pepe_in_the_well/doc/design/js/delete.js
@@ -0,0 +1,21 @@
+// 딜레이 모달용 변수
+const modal = document.querySelector("#my-modal");
+const modalImg = document.querySelector(".modal-img");
+
+// form submit시 동작 함수
+function chkDelayModal(event) {
+  event.preventDefault(); // 일단 폼전송 멈춤
+
+  // 이미지 생성
+  const img = new Image();
+  img.src = "./img/pcons/disappear_pepe.gif";
+
+  // 모달 보여줌
+  modal.style.display = "block";
+  modalImg.appendChild(img); // 이미지 삽입
+
+  // 3.2초(3200ms) 딜레이 후 폼제출
+  setTimeout(() => {
+    event.target.submit();
+  }, 3200);
+}
