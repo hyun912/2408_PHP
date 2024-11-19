4b616d6b50c15a4c879c94fa7acc5807d20a1a69
hyun912 <kim374523@naver.com>
2024-11-19 09:32:19 +0900

    241118 작업분

---
+++ vue/vue_edu/src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <!-- HTML -->
+
+  <!-- v-if -->
+  <h1 v-if="cnt >= 5">5보다 큼</h1>
+  <h1 v-else-if="cnt < 0">음수</h1>
+  <h1 v-else>나머지</h1>
+  
+  <h1>{{ cnt }}</h1>
+  <button @click="addCnt">증가</button> &nbsp;
+  <button @click="disCnt">감소</button>
+  
+  <hr>
+  
+  <!-- class 바인딩 -->
+  <h2>이름: {{ userInfo.name }}</h2>
+  <h2 :class="ageBlue">나이: {{ userInfo.age }}</h2>
+  <h2>성별: {{ userInfo.gender }}</h2>
+  <button @click="changeName">변경</button> &nbsp;
+  <button @click="changeAgeBlue">나이 파랑</button> 
+  
+  <hr>
+  <!-- v-model 양방향 데이터 바인딩, 회원가입의 실시간 체크때 쓰임 -->
+  <input type="text" v-model="transGender"> &nbsp;
+  <button @click="userInfo.gender = transGender">성별 변경</button>
+  <p>{{ transGender }}</p>
+
+  <hr>
+
+  <!-- v-show, 주로 토글용 모달에 씀 -->
+  <h1 v-show="flgShow">출력</h1>
+  <button @click="flgShow = !flgShow">쇼 토글</button> &nbsp;
+
+  <hr>
+
+  <!-- v-for -->
+  <!-- <div v-for="i in 8" :key="i++">
+    ** {{ i }} 단 **
+    <div v-for="j in 9" :key="j">
+      {{ `${i} * ${j} = ${i * j}` }}
+    </div>
+  </div> -->
+
+  <div v-for="(item, key) in data" :key="item">
+    <p>{{`${key} : ${item.name} - ${item.age} - ${item.gender}`}}</p>
+  </div>
+
+  <hr>
+
+  <!-- 자식 컴포넌트 호출, 이름을 두단어 이상 조합 필수  -->
+  <BoardComponent />
+</template>
+
+<script setup>
+import BoardComponent from './components/BoardComponent.vue';
+import { reactive, ref } from 'vue';
+
+  // JS
+  const cnt = ref(0);
+  const userInfo = reactive({ // 배열과 같음
+    name: '홍'
+    ,age: 20
+    ,gender: 'undefined'
+  });
+  const ageBlue = ref('');
+  const transGender = ref('');
+  const flgShow = ref('');
+  const data = reactive(
+    [
+      {name: '홍' ,age: 20 ,gender: '남'},
+      {name: '김' ,age: 12 ,gender: '여'},
+      {name: '둘' ,age: 41 ,gender: '수'},
+    ]
+  );
+
+  function addCnt() {
+    cnt.value++;
+  }
+
+  function disCnt() {
+    cnt.value--;
+  }
+
+  function changeName() {
+    userInfo.name = '갑';
+  }
+
+  function changeAgeBlue() {
+    ageBlue.value = 'blue';
+  }
+  
+</script>
+
+<style>
+  /* CSS */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.blue {
+  color: #0000ff;
+}
+</style>
---
+++ vue/vue_edu/src/components/BoardComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- 인라인 핸들러 안씀 -->
+    <!-- <h1 @click="plusCnt">{{ cnt }}</h1> -->
+    <h1 @mouseenter="plusCnt">{{ cnt }}</h1>
+    
+    <hr>
+    
+    <input type="text" v-model="tel"> &nbsp;
+    <span :style="color">{{ msg }}</span>
+  </div>
+</template>
+<script setup>
+import { ref, watch } from 'vue';
+
+  const cnt = ref(0);
+
+  // 대부분 메소드 핸들러로 씀
+  const plusCnt = () => {
+    cnt.value++;
+  }
+
+  // 와처즈 : 값이 변경되면 즉시실행
+  const tel = ref('');
+  const msg = ref('X');
+  const color = ref('color:red');
+
+  watch(tel, (val) => {
+    const regex = /^[0-9]{10,11}$/;
+    if(!regex.test(val)) {
+      msg.value = 'X';
+      color.value = 'color:red;';
+    } else {
+      msg.value = 'O';
+      color.value = 'color:green;';
+    }
+  });
+
+</script>
+
+<style>
+</style>
