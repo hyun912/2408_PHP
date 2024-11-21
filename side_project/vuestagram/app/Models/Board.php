3ebcd6f73ab8fd37b52de49a08e92eb33aa07a70
hyun912 <kim374523@naver.com>
2024-11-21 15:31:53 +0900

    241121 중간저장

---
+++ side_project/vuestagram/app/Models/Board.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Board extends Model {
+  use HasFactory, SoftDeletes;
+
+  protected $primaryKey = 'board_id';
+
+  protected $fillable = [
+    'user_id',
+    'content',
+    'img',
+    'like',
+  ];
+
+  public function serializeDate(DateTimeInterface $date) {
+    return $date->format('Y-m-d H:i:s');
+  }
+}
--- side_project/vuestagram/app/Models/User.php
+++ side_project/vuestagram/app/Models/User.php
@@ -2,43 +2,38 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  protected $primaryKey = 'user_id';
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  protected $fillable = [
+    'account',
+    'name',
+    'password',
+    'gender',
+    'profile',
+    'refresh_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  // 인증 관련 숨겨야 되는 컬럼
+  protected $hidden = [
+    'password',
+    'refresh_token',
+  ];
+  
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  public function serializeDate(DateTimeInterface $date) {
+    return $date->format('Y-m-d H:i:s');
+  }
 }
