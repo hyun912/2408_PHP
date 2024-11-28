a445d1550b1d8d5c990ac6c7384d768e93e1940b
hyun912 <kim374523@naver.com>
2024-11-28 17:28:00 +0900

    241128 짭스타그렘 생성

--- side_project/vuestagram/routes/api.php
+++ side_project/vuestagram/routes/api.php
@@ -2,14 +2,18 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BoardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/registration', [UserController::class, 'store'])->name('user.store');
 
 // 인증이 필요한 그룹
 Route::middleware('my.auth')->group(function() {
-  // 로그아웃 인증
+  // 인증 관련
   Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+  Route::post('/reissue', [AuthController::class, 'reissue'])->name('auth.reissue');
+  
 
   // 게시판 관련
   Route::get('/boards', [BoardController::class, 'index'])->name('boards.index');
